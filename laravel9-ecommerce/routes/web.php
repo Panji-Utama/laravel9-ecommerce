f6798c1336e130085fa78d612a4c521792c570b5
FIRMAN PANJI <fpu2468@gmail.com>
2023-10-16 19:09:56 +0700

    feat: Routing

--- laravel9-ecommerce/routes/web.php
+++ laravel9-ecommerce/routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// EP = End Point
+// V = View
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+// EP contact, V contact, fixed/static parameter
+Route::view('/contact', 'contact', ['name' => 'Panji', 'phone' => '0812xxxxxxxx']);
+
+// Redirecting
+Route::get('contact-mock', function () {
+    return "lolololol";
+});
+Route::redirect('/contact2', '/contact-mock');
+
+// Parameter URL
+Route::get('users/{id}', function ($id) {
+    return 'User dengan id : '.$id;
+});
+
+// Pass param to view 
+Route::get('product/{id}', function ($id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+// Grouping/prefix
+Route::prefix('administrator')->group(function() {
+    Route::get('/admin-profile', function() {
+        return 'admin.profile';
+    });
+    Route::get('/admin-contact', function() {
+        return 'admin.contact';
+    });
 });
